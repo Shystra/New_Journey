5ba16d90ea735729d899016651e6d50f6448a9ee
Shystra <jlsp.2019@hotmail.com>
2023-06-05 21:16:37 -0300

    apenas tratando dados

---
+++ conceitos.js
@@ -0,0 +1,36 @@
+const array = [5, 8, 10, 20, 30, 100];
+const object = {}
+
+const verifyNumber = array.forEach(( param1 ) => {
+    // console.log( 'param1 =>', param1 );
+    // console.log( 'param2 =>', param2 );
+    // console.log( 'param2 =>', param3 );
+    return param1 = param1
+    
+});
+
+const changeNumber = array.map( n => n * 2)
+console.log(changeNumber)
+
+// console.log(verifyNumber)
+
+const recebe = []
+
+const acrescenta = recebe.push(changeNumber)
+
+console.log(recebe)
+ 
+
+
+
+
+const ifNumber = array.forEach((value, index) => {
+    let aux = 0; 
+    if (value > aux){
+        aux = value;
+        // console.log(aux);
+    }
+});
+
+
+
