c844699e6c2e39b9c0a38a118cafd0ee92873c6a
Shystra <jlsp.2019@hotmail.com>
2023-06-06 20:10:37 -0300

    just formatations

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@ const http = require('http');
 
 const app = http.createServer((request, response) => {
     console.log( request.method, request.url );
+    const users = [
+        {
+            id: 1,
+            name: 'Lucas',
+        },
+        {
+            id: 2,
+            name: 'Souza',
+        },
+        {
+            id: 3,
+            name: 'Pereira'
+        }
+    ];
+
 
     if (request.method === 'GET' && request.url === '/'){
-        response.write('First request');
+        response.writeHead(200, {'Content-Type' : 'application/json'})
+        response.end(JSON.stringify(users));
+        
 
     }   else{
         response.writeHead(404, { "Content-Type" : 'text/plain' });
