f69a4868ff9c60ef1b57355136c066727737c91b
Shystra <jlsp.2019@hotmail.com>
2023-06-06 19:55:52 -0300

    tesing error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const http = require('http');
 
 const app = http.createServer((request, response) => {
-    response.write('Hello');
-    response.end();
+    console.log( request.method, request.url );
+
+    if (request.method === 'GET' && request.url === '/'){
+        response.write('First request');
+
+    }   else{
+        response.writeHead(404, { "Content-Type" : 'text/plain' });
+        response.end(`Cannot ${request.method} ${request.url}`)
+    }
+
 });
 
 app.listen(3000, () => console.log('Server is running'))
