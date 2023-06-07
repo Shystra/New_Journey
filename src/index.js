bc8cde870285183f1a646d38d2b6cb160054c1e0
Shystra <jlsp.2019@hotmail.com>
2023-06-06 21:56:13 -0300

    using insominia

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 const express = require("express");
-
+const  { v4: uuid } = require('uuid');
 const app = express()
 app.use(express.json());
 
+const user = []
+
+app.post('/users', (request, response) => {
+    const { name, email } = request.body;
+
+    user.push({
+        name,
+        email, 
+        id: uuid(),
+    });
+    return response.status(201).json({message: 'Ok'})
 
 
+});
 
 
 app.listen(3333, () => console.log('Server is running'))
